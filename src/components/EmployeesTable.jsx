2d45170fab718ed97c7339e21b76f414e77cb676
ajmal31 <ajmalmuhammed846@gmail.com>
2024-10-15 18:05:36 +0530

    Feat : employee detaila API & table view

--- src/components/EmployeesTable.jsx
+++ src/components/EmployeesTable.jsx
@@ -1,27 +1,43 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/const";
+import { useEffect, useState } from "react";
+
 const EmployeesTable = () => {
+  const [employeeDetails, setEmployeeDetails] = useState([]);
+  
+  const fetchData = async () => {
+    const response = await axios.get(`${BASE_URL}employee`);
+    setEmployeeDetails(response?.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-    <table className="table-auto border border-black w-full">
-      <thead>
-        <tr className="">
-          <th className="border border-black" >Employee Name</th>
-          <th  className="border border-black">Address</th>
-          <th  className="border border-black">Department</th>
-          <th  className="border border-black" >Department Description</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td  className="border border-black" >Muhamed Ajmal</td>
-          <td  className="border border-black">Mannarkkad, Kerala</td>
-          <td  className="border border-black">Police</td>
-          <td  className="border border-black">Police department</td>
-        </tr>
-      </tbody>
-    </table>
-  </>
-  
+      <table className="table-auto border border-black w-full">
+        <thead>
+          <tr className="">
+            <th className="border border-black">Employee Name</th>
+            <th className="border border-black">Address</th>
+            <th className="border border-black">Department</th>
+            <th className="border border-black">Department Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employeeDetails.map((val) => (
+            <tr>
+              <td className="border border-black">{val?.name}</td>
+              <td className="border border-black">{val?.address}</td>
+              <td className="border border-black">{val?.department?.name}</td>
+              <td className="border border-black">{val?.department?.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
-export default EmployeesTable
+export default EmployeesTable;
